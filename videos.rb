93f1bfa0477c489b50f397c9d32c8811ad45c049
jordanbudi <jordanbudi@gmail.com>
2016-06-15 17:22:57 -0400

    updated videos.rb to reflect videos not movies

--- videos.rb
+++ videos.rb
@@ -1,14 +1,14 @@
-#1. Define a method first_movie that return the first movie in the queue
-# This method should accept an array as an argument (the movie queue)
+#1. Define a method first_video that return the first video in the queue
+# This method should accept an array as an argument (the video queue)
 
-#2. Define a method watch_movie that deletes the first movie in the array and returns the modified array
-#This method should accept an array as an argument (the movie queue)
+#2. Define a method watch_video that deletes the first video in the array and returns the modified array
+#This method should accept an array as an argument (the video queue)
 
 #3. Define a method update_queue that returns the updated array
-# This method takes two arguments (the array of movies and the movie you want to add to the queue)
+# This method takes two arguments (the array of videos and the video you want to add to the queue)
 
-#4. Define a method view_queue that uses the each method to iterate over the array of movies
-# This method should use puts to print out "You will watch <movie name>" for every movie in the list
-#This method should accept an array as an argument (the movie queue)
+#4. Define a method view_queue that uses the each method to iterate over the array of videos
+# This method should use puts to print out "You will watch <video name>" for every video in the list
+#This method should accept an array as an argument (the video queue)
 
 
