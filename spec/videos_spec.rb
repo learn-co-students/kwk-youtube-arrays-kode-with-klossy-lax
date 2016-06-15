613d3a6c305da31d52b6a2e1eb3f4a697e7d491a
jordanbudi <jordanbudi@gmail.com>
2016-06-15 17:39:58 -0400

    change word queue to playlist

--- spec/videos_spec.rb
+++ spec/videos_spec.rb
@@ -19,15 +19,15 @@ describe 'videos' do
     end
   end
 
-  describe '#update_queue' do
-    it 'adds videos to the end of your queue' do
-      expect(update_queue(@videos, "Why can't girls code? They're beautiful.")).to eq ["PSY - GANGNAM STYLE", "Wiz Khalifa - See You Again ft. Charlie Puth", "Mark Ronson - Uptown Funk ft. Bruno Mars", "Taylor Swift - Blank Space", "Justin Bieber - Sorry", "How To Apply | Kode With Klossy", "Why can't girls code? They're beautiful."]
+  describe '#update_playlist' do
+    it 'adds videos to the end of your playlist' do
+      expect(update_playlist(@videos, "Why can't girls code? They're beautiful.")).to eq ["PSY - GANGNAM STYLE", "Wiz Khalifa - See You Again ft. Charlie Puth", "Mark Ronson - Uptown Funk ft. Bruno Mars", "Taylor Swift - Blank Space", "Justin Bieber - Sorry", "How To Apply | Kode With Klossy", "Why can't girls code? They're beautiful."]
     end
   end
 
-  describe '#view_queue' do
-    it 'prints out the videos in the queue using the each method' do
-      output = capture_stdout { view_queue(@videos) }
+  describe '#view_playlist' do
+    it 'prints out the videos in the playlist using the each method' do
+      output = capture_stdout { view_playlist(@videos) }
       expect(output).to eq "You will watch PSY - GANGNAM STYLE\nYou will watch Wiz Khalifa - See You Again ft. Charlie Puth\nYou will watch Mark Ronson - Uptown Funk ft. Bruno Mars\nYou will watch Taylor Swift - Blank Space\nYou will watch Justin Bieber - Sorry\nYou will watch How To Apply | Kode With Klossy\n"
     end
   end
--- videos.rb
+++ videos.rb
@@ -1,14 +1,14 @@
-#1. Define a method first_video that return the first video in the queue
-# This method should accept an array as an argument (the video queue)
+#1. Define a method first_video that return the first video in the playlist
+# This method should accept an array as an argument (the video playlist)
 
 #2. Define a method watch_video that deletes the first video in the array and returns the modified array
-#This method should accept an array as an argument (the video queue)
+#This method should accept an array as an argument (the video playlist)
 
-#3. Define a method update_queue that returns the updated array
-# This method takes two arguments (the array of videos and the video you want to add to the queue)
+#3. Define a method update_playlist that returns the updated array
+# This method takes two arguments (the array of videos and the video you want to add to the playlist)
 
-#4. Define a method view_queue that uses the each method to iterate over the array of videos
+#4. Define a method view_playlist that uses the each method to iterate over the array of videos
 # This method should use puts to print out "You will watch <video name>" for every video in the list
-#This method should accept an array as an argument (the video queue)
+#This method should accept an array as an argument (the video playlist)
 
 
