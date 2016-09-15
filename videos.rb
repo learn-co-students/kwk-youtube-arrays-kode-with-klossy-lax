1fec50843631d1de5e79c48e250412c6c52d4ee9
sofia ongele <sofia.ongele@ealas.org>
2016-09-15 00:10:38 +0000

    Done.

--- videos.rb
+++ videos.rb
@@ -27,33 +27,38 @@ klossy = [
 
 "Angel Wings for Two: A Photoshoot w/ Make-A-Wish | Karlie Kloss"
 ]
+
+videos = ["PSY - GANGNAM STYLE",
+"Wiz Khalifa - See You Again ft. Charlie Puth",
+"Mark Ronson - Uptown Funk ft. Bruno Mars",
+"Taylor Swift - Blank Space",
+"Justin Bieber - Sorry",
+"How To Apply | Kode With Klossy" ]
 #1. Define a method first_video that return the first video in the playlist
 # This method should accept an array as an argument (the video playlist)
 def first_video(playlist)
   playlist.first
 end
-puts first_video(klossy)
+puts first_video(videos)
 #2. Define a method watch_video that deletes the first video in the array and returns the modified array
 #This method should accept an array as an argument (the video playlist)
 def watch_video(playlist)
   playlist.delete_at(0)
   playlist
 end
-puts watch_video(klossy)
+puts watch_video(videos)
 #3. Define a method update_playlist that returns the updated array
 # This method takes two arguments (the array of videos and the video you want to add to the playlist)
 def update_playlist(playlist, video)
   playlist.push(video)
 end
-puts update_playlist(klossy, "Throwing the First Pitch | Karlie Kloss")
+puts update_playlist(videos, "Why can't girls code? They're beautiful.")
 #4. Define a method view_playlist that uses the each method to iterate over the array of videos
 # This method should use puts to print out "You will watch <video name>" for every video in the list
 #This method should accept an array as an argument (the video playlist)
 def view_playlist(playlist)
   playlist.each do |key|
-    puts "you will watch #{key}"
+    puts "You will watch #{key}"
   end
 end
-puts view_playlist(klossy)
-
-
+puts view_playlist(videos)
